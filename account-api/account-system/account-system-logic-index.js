d8b3b6c48723da695f9bc9f075755f97a70b0f86
Misticalkai <realmisticalkai@gmail.com>
2024-12-28 00:12:57 -0600

    Fixed some more library issues

--- account-api/account-system/account-system-logic-index.js
+++ account-api/account-system/account-system-logic-index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { Pool } = require("pg");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
