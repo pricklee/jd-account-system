a309a414f5fcd31f907c4f4e9a9b01cd9ac18107
Misticalkai <realmisticalkai@gmail.com>
2024-12-30 11:11:55 -0600

    Fixed a issue with profanityb filters

--- account-api/account-system/account-system-logic-index.js
+++ account-api/account-system/account-system-logic-index.js
@@ -22,8 +22,8 @@ const pool = new Pool({
   const bannedWords = ["nigger", "ass", "nigga", "niga", "nig", "niger", "fuck", "fag", "fagget", "boob", "dick", "bastard", "faggot", "retard", "penis", "slut", "tit", "tits", "fucker", "nazi", "isis", "sex", "rape", "porn", "pornhub", "xnxx", "xvideos", "xhamsters", "pussy", "vagina", "r34", "rule34", "genocide", "trany", "tranny", "tranni", "trani", "f@g", "r@pe", "b00b", "misticalkai", "pricklety", "jammerdash", "automoderator", "hizuru_chan", "hizuru", "n-word", "k-word", "kike", "chink", "ch1nk", "ch!nk", "dyke", "shemale", "she-male", "shemale", "she-male", "p0rn", "porno", "p0rno", "anus", "genitals", "cock", "cocks", "c0ck", "c0cks"]
 
   const containsProfanity = (text) => {
-    const lowerCaseText = text.toLowerCase();
-    return bannedWords.com((word) => lowerCaseText.includes(word));
+      const lowerCaseText = text.toLowerCase();
+      return bannedWords.some((word) => lowerCaseText.includes(word));
   };
 
 // Role Permissions Configuration
