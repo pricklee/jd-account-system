a0e30561c4ae7d1f69a7136a0de0d6568668d2c2
Misticalkai <realmisticalkai@gmail.com>
2024-12-30 16:32:54 -0600

    Fixing more issues

--- account-api/account-system/account-system-logic-index.js
+++ account-api/account-system/account-system-logic-index.js
@@ -11,18 +11,18 @@ const app = express();
 app.use(express.json());
 
 // IP logging
-app,use((req, res, next) => {
-  req.clientIp = req.headers['x-forwared-for'] || req.connection.remoteAddress;
+app.use((req, res, next) => {
+  req.clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   next();
 });
 
 // Postgres Connection Pool
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
   const bannedWords = ["nigger", "ass", "nigga", "niga", "nig", "niger", "fuck", "fag", "fagget", "boob", "dick", "bastard", "faggot", "retard", "penis", "slut", "tit", "tits", "fucker", "nazi", "isis", "sex", "rape", "porn", "pornhub", "xnxx", "xvideos", "xhamsters", "pussy", "vagina", "r34", "rule34", "genocide", "trany", "tranny", "tranni", "trani", "f@g", "r@pe", "b00b", "misticalkai", "pricklety", "jammerdash", "automoderator", "hizuru_chan", "hizuru", "n-word", "k-word", "kike", "chink", "ch1nk", "ch!nk", "dyke", "shemale", "she-male", "shemale", "she-male", "p0rn", "porno", "p0rno", "anus", "genitals", "cock", "cocks", "c0ck", "c0cks"]
 
